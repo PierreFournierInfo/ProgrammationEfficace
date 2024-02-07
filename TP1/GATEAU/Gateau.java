893b616d32330b0ec22d88a809081d8faa7b0fb4
Pierre fournier <terpy2002.pf@gmail.com>
2024-02-07 23:54:44 +0100

    TP1 parseur + résolution

--- TP1/GATEAU/Gateau.java
+++ TP1/GATEAU/Gateau.java
@@ -7,34 +7,43 @@ import java.nio.file.Paths;
 
 public class Gateau {
    
-    private static final String FILENAME = "1.in";
-    private static String [][] tableau;
-    public static void main(String[] args) {
-        BufferedReader bufferedreader = null;
-        FileReader filereader = null;
-        String [] tab;
+    private static String FILENAME ;
+    private static String [][] tabString;
+    private static int [][] tabInt;
 
-        //System.out.println("Hello worlds");
+    public static void main(String[] args) {
+
         try {
-            
-            filereader = new FileReader(FILENAME);
-            bufferedreader = new BufferedReader(filereader);
+            //**********************On lit l'adresse l'adresse du fichier passé en argument*********************************
+            FILENAME=args[0];
+            FileReader filereader = new FileReader(FILENAME);
+            BufferedReader bufferedreader = new BufferedReader(filereader);
             String strCurrentLine = bufferedreader.readLine();
+
+            //*************On lit ligne par ligne et les stocke dans un tableau de String**********************
+            String [] tab;
             tab=new String[Integer.parseInt(strCurrentLine)];
             int z=0;
             while ((strCurrentLine = bufferedreader.readLine()) != null) {
                 tab[z]=strCurrentLine;
-                System.out.println(strCurrentLine);
                 z++;
             }
-            System.out.println();
-            System.out.println("tableau finale: \n");
 
-            tableau=parse(tab);
-            afficheTab(tableau);
-            System.out.println("nombre de lignes " +tableau.length);
-            System.out.println("nombre de mots a la ligne 5:  " +tableau[5].length);
+            //******On sépare les mots de chaque ligne dans un tableau de tableau de String ****************/
+            tabString=parse(tab);
+            
+            //******On converti le tableau de tableau de String en Tableau de Tableau de Int ***************/
+            tabInt=convert(tabString);
+            afficheTab(tabInt);
 
+            //*********On trouve la solution au problème et la renvoie dans un fichier de sortie *******************/
+
+            //-
+            int nombreRecettes=solution();
+            System.out.println("\n Nombre de recettes possibles "+nombreRecettes);
+
+            //-
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -72,6 +81,17 @@ public class Gateau {
         }
         return res;
     }
+    
+    public static int[][] convert(String [][] tableau){
+        int [][] tab=new int[tableau.length][];
+        for(int i=0;i<tableau.length;i++){
+            tab[i]=new int [tableau[i].length];
+            for(int j=0;j<tableau[i].length;j++){
+                tab[i][j]=Integer.parseInt(tableau[i][j]);
+            }
+        }
+        return tab;
+    }
 
     public static void afficheTab(String [][]tab){
         for(int i=0;i<tab.length;i++){
@@ -82,20 +102,32 @@ public class Gateau {
             System.out.println();
         }
     }
-
-
-
-    public class Paire{
-        private int quantite;
-        private int stock;
-        public Paire( int q, int s){
-            quantite=q;
-            stock=s;
+    public static void afficheTab(int [][]tab){
+        for(int i=0;i<tab.length;i++){
+            
+            for(int j=0;j<tab[i].length;j++){
+                System.out.print(tab [i][j]+" ");
+            }
+            System.out.println();
         }
     }
 
-    public class Recette{
-        
+    public static int solution (){
+        int min=tabInt[0][1];
+        for(int i=0;i<tabString.length;i++){
+            if(tabInt[i][0]==0){
+                return 0;
+            }
+            int minLoc=(tabInt[i][1]/tabInt[i][0]);
+            if(minLoc<min){
+                min=minLoc;
+            }
+
+        }
+        return min;
     }
+
+
+
 
 }
