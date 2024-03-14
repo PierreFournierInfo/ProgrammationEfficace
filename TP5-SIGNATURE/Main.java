4dac67de01a363860667c573caedf8da481be2ba
RICHARD guives <guives.richard@gmail.com>
2024-03-14 12:47:07 +0100

    Solution Main + test.sh

--- TP5-SIGNATURE/Main.java
+++ TP5-SIGNATURE/Main.java
@@ -9,29 +9,19 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class Main{
-
-	private static String ligne;
+public class Main {
+    private static String ligne;
     private static char[] alpha; /*{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}; */
-    private static String[] tab;
-    private static int taille;
-
+    private static ArrayList<String> sc = new ArrayList<>();
+    private static String sol = "";
 
 	@SuppressWarnings("unchecked")
     public static void parse(String file){
     	try {
             FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            alpha = new char[26];
-            for(int i = 0; i < 26; i++){
-                alpha[i] = (char)(65 + i);
-            }
-		
 
            	ligne = bufferedReader.readLine();
-
-            taille=ligne.length();
 
             bufferedReader.close();
             fileReader.close();
@@ -39,71 +29,54 @@ public class Main{
             e.printStackTrace();
         }
     }
+
+    private static int nbOcc(String mot){
+        int res = 0;
+        for(String s : sc){
+            if(mot.equals(s))res++;
+        }
+        return res;
+    }
+
+    private static String min(){
+        String res = sc.get(0);
+        for(String s : sc){
+            if(!res.equals(s)){
+                if(nbOcc(s) < nbOcc(res))res = s;
+            }
+        }
+        return res;
+    }
+
+    private static String sousChaine(int deb, int fin){
+        String res = "";
+        for(int i = deb; i < fin; i++){
+            if(i < ligne.length())res = res + ligne.charAt(i);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
+        alpha = new char[26];
+        for(int i = 0; i < 26; i++){
+            alpha[i] = (char)(65 + i);
+        }
 		parse(args[0]);
-        taille=ligne.length();
 
-		System.out.println(ligne);
-        taille=ligne.length();
-        tab=new String[taille];
-        //*********On trouve la solution au problème et la renvoie dans un fichier de sortie *******************/
-        String resultat=recherche();
-           
-
-        //System.out.println("\n Nombre de recettes possibles "+nombreRecettes);
-        try{
-            String nomFichier =  args[0]+".out";
-            FileWriter fileWriter = new FileWriter(nomFichier, true);
-            PrintWriter printWriter = new PrintWriter(fileWriter);
-
-            printWriter.println("Signature : " + resultat ); // Écrit le paramètre dans le fichier
-            printWriter.close(); // Ferme le PrintWriter pour libérer les ressources
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-	}
-
-    public static String recherche(){
-        for(int a=0;a<taille;a++){
-            tab[a]=String.valueOf(ligne.charAt(a));
-            
-        }
-        if (Compare(taille)!="Echec"){
-            return Compare(taille);
-        }
-        for(int i=0; i<taille-1;i++){
-            
-            for(int j=0;j<taille-1;j++){
-                if(i+tab[i].length()<taille){ //REVOIR POTENTIELLEMENT ICI
-                    tab[i]+=ligne.charAt(j);
-                    
-                }
+        boolean fini = false;
+        int pas = 1;
+        String scmin = "";
+        while(!fini){
+            for(int i = 0; i < ligne.length(); i++){
+                String s = sousChaine(i, i+pas);
+                //System.out.println(s);
+                sc.add(s);
             }
-            if (Compare(taille)!="Echec"){
-                return Compare(taille);
-            }
+            scmin = min();
+            //System.out.println(scmin);
+            if(nbOcc(scmin) == 1)fini = true;
+            pas++;
         }
-        return ligne;
-    }
-
-    public static String Compare( int n){
-        boolean b=false;
-        for(int i=0; i<n; i++){
-            b=false;
-            System.out.println("c");
-            for(int j=0; j<n; j++){
-                if(i!=j && tab[i].equals(tab[j])){
-                    b=true;
-                }
-            }
-            if(b==false){
-                return tab[i];
-            }
-            b=false;
-        }
-        return "ECHEC";
-    }
-
-	
+        System.out.println(scmin);
+	}  
 }
