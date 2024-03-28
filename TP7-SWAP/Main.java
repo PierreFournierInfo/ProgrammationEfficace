4cea24e9d9e2346d4e08a9427684df6c7688d9e8
RICHARD guives <guives.richard@gmail.com>
2024-03-28 12:51:26 +0100

    Merge Zoe + Maxime into Main

--- TP7-SWAP/Main.java
+++ TP7-SWAP/Main.java
@@ -12,6 +12,8 @@ import java.util.List;
 public class Main {
     private static int nbrDisque;
     private static ArrayList<Disque> listDisque = new ArrayList<>();
+    private static int dispo = 0;
+    private static int achete = 0;
 
 	@SuppressWarnings("unchecked")
     public static void parse(String file){
@@ -41,6 +43,45 @@ public class Main {
         }
     }
 
+    public static ArrayList<Disque> separe(ArrayList<Disque> d){
+        ArrayList<Disque> t = new ArrayList<>();
+        for (Disque disque : d) {
+            if(disque.avant < disque.apres){
+                if(t.isEmpty()){
+                    t.add(disque);
+                }
+                else{
+                    for (Disque disque2 : t) {
+                        if(disque.avant < disque2.avant){
+                            t.add((t.indexOf(disque2)),disque);
+                            break;
+                        }
+                    }
+                }
+                d.remove(disque);
+            }
+        }
+        return t;
+    }
+
+    public static void phase1(){
+        ArrayList<Disque> l = separe(listDisque);
+        int n = l.size();//récupérer la taille de la liste triée de maxime
+        for(int i=0; i<n; i++){
+            if(i==0){
+                achete+=l.get(i).avant;
+                dispo=l.get(i).apres;
+            }else{
+                if(dispo<l.get(i).avant){
+                    achete+=l.get(i).avant-dispo;
+                    dispo+=l.get(i).apres;
+                }else{
+                    dispo+=(l.get(i).apres-l.get(i).avant);
+                }
+            }
+        }
+    }
+
     public static void printList(){
         for ( Disque d : listDisque ){
             System.out.println(d.avant + " " + d.apres);
@@ -52,5 +93,7 @@ public class Main {
         parse(args[0]);
 
         printList();
+
+        System.out.println(achete);
 	}  
 }
