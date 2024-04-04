0c66d3585d21184122c35cabd7175b30e6af9ae5
SEJOURNE maxime <sejournm@informatique.univ-paris-diderot.fr>
2024-04-04 12:38:51 +0200

    Update Maxime.java

--- TP8-PETROLE/Maxime.java
+++ TP8-PETROLE/Maxime.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
+
 public class Maxime {
-    public int relie(int x1, int y1, int x2, int y2, Gisement[] l){
-        double a = (y1-y2)/(x1 - x2);
-        double b = y1 - a * x1;
+    public static int relie(int x1, int y1, int x2, int y2, ArrayList<Gisement> listGisements){
+        if(x1 - x2 ==0)
+            return 0;
+        
+        double a = ((double)y1-(double)y2)/((double)x1 - (double)x2);
+        double b = (double)y1 - (double)a * (double)x1;
         int petrole = 0;
-        for (Gisement ligne : l) {
-            double x = (ligne.y-b)/a;
+        for (Gisement ligne : listGisements) {
+            double x = ((double)ligne.y-(double)b)/(double)a;
             if((ligne.x1 <= x && ligne.x2 >= x)||(ligne.x1 >= x && ligne.x2 <= x))
                 petrole += ligne.qt;
         }
